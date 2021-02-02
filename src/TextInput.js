5c6e6f69649ebfa4cd6ca55e5b487bc691d7012e
knguye1999 <55815753+knguye1999@users.noreply.github.com>
2021-02-01 19:01:09 -0800

    Add files via upload

---
+++ src/TextInput.js
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+
+function TextInput(props) {
+  const [text, setText] = useState('')
+
+  function sendMessage(){
+    if(text==='') return // skip the function
+    props.send(text)
+    setText('')
+  }
+
+  function keyPressed(e){
+    if(e.key==='Enter') {
+      sendMessage()
+    }
+  }
+
+  return <footer className="text-input">
+    
+    <input className="input"
+      placeholder="POKE YOUR FRIEND"
+      value={text}
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={keyPressed}
+    />
+    <button onClick={sendMessage} disabled={!text} className="send-button">
+      ↑
+    </button>
+  </footer>
+}
+
+export default TextInput
