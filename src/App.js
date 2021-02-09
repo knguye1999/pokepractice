6003e32d60dac68f09b41bf44956283db86ddf09
knguye1999 <55815753+knguye1999@users.noreply.github.com>
2021-02-08 21:47:05 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 import TextInput from './TextInput';
 import Message from './Message'
+import NamePicker from './NamePicker'
+import {db, useDB} from './db'
 
 function App() {
-  const [messages,setMessages] = useState([])
+  const messages = useDB()
+  const [username,setUsername] = useState(
+    localStorage.getItem('username') || ''
+  )
 
+  console.log(messages)
   return <div className="App">
  
     <header className="header">
       <div className="logo" />
       POKE
+      <NamePicker saveName={setUsername} />
     </header>
 
     <main className="messages">
-    {messages.map((m,i)=> {
-   return <Message key={i} {...m} />
-})}
+      {messages.map((msg,i)=> {
+        const isMe = msg.name===username
+        return <Message key={i} {...msg} isMe={isMe} />
+      })}
     </main>
 
     <TextInput 
-      send={(t)=> setMessages( [...messages, {text:t}] )}
+      send={(t)=> db.send({text:t, name:username, date:new Date()})}
     />
 
   </div>
